b616824583f413303105a7994be5b4f33133215d
Justin Hutsell <hutselljustin@gmail.com>
2019-07-30 15:18:33 -0400

    adds controller actions

--- backend/app/controllers/stocks_controller.rb
+++ backend/app/controllers/stocks_controller.rb
@@ -1,2 +1,29 @@
 class StocksController < ApplicationController
+
+    def index
+        @stocks = Stock.all 
+        render json: @stocks
+    end
+
+    def show
+        @stock = Stock.find(params[:id])
+        render json: @stock
+    end
+
+    def create
+        @stock = Stock.create(stock_params)
+        if @stock.valid?
+            render json: @stock
+        else
+            render json: { errors: @stock.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+
+    private 
+
+    def stock_params
+        params.require(:stock).permit(:name, :sector, :company_name, :exchange, :industry, :website, :image_url)
+    end
+
 end
--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,2 +1,29 @@
 class UsersController < ApplicationController
+
+    def index
+        @users = User.all 
+        render json: @users
+    end
+
+    def show
+        @user = User.find(params[:id])
+        render json: @user
+    end
+
+    def create
+        @user = User.create(user_params)
+        if @user.valid?
+            render json: @user
+        else
+            render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+
+    private 
+
+    def user_params
+        params.require(:user).permit(:name)
+    end
+
 end
